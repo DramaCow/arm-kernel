db44a818691334f06e0b00b0b7fbbc372466c466
SamC <sam.coward@ntlworld.com>
2016-03-22 10:57:12 +0000

    removed accidental duplicate definitions

--- lab-3_e/disk.sh
+++
@@ -1,1 +0,0 @@
-python device/disk.py --host=127.0.0.1 --port=1235 --file=device/disk.bin --block-num=2048 --block-len=512
--- lab-3_e/newdisk.sh
+++
@@ -1,3 +0,0 @@
-dd of=disk.bin if=/dev/zero count=1048576 bs=1
-rm device/disk.bin
-mv disk.bin device
